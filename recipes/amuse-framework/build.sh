b1b3ac2a332d4cf6d07e8e97481ee3753b79bf4c
Lourens Veen <l.veen@esciencecenter.nl>
2024-06-04 12:34:08 +0200

    Upgrade to new build system, WIP

--- recipes/amuse-framework/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd packages/amuse-framework
-cp $BUILD_PREFIX/share/gnuconfig/config.* .
-./configure
-${PYTHON} setup.py sdist
-${PYTHON} -m pip install -vv --no-clean dist/*.tar.gz
-
--- recipes/amuse-ph4/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd packages/amuse-ph4
-${PYTHON} setup.py sdist
-${PYTHON} -m pip install -vv --no-clean dist/*.tar.gz
-
